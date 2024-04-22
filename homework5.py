5c58eee8f0849652c6912b6f6681dc6779ec6741
AlRogov <166138869+AlRogov@users.noreply.github.com>
2024-04-23 01:25:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi']
+print(my_list)
+print(my_list[0])
+print(my_list[3])
+print(my_list[2:4])
+my_list[2] = 'grape'
+print(my_list)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print(my_dict)
+print(my_dict['orange'])
+my_dict['kiwi'] = 'киви'
+print(my_dict)
